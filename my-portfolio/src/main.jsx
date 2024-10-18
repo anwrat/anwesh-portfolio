01143f6ea0a1166dbf766f600ee095e7a5f7e206
anwrat <anweshrwt22@gmail.com>
2024-10-18 07:35:25 +0545

    Clearing some things

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Importing fonts
+import '@fontsource/outfit'
+import '@fontsource/roboto'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
