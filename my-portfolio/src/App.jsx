3152e1dcb211ef7b8b18240b55e88e7abccbc347
anwrat <anweshrwt22@gmail.com>
2024-10-24 07:24:24 +0545

    importing CSS variables from var.css into App.module.css

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.module.css'
+import styles from'./App.module.css'
 
 function App() {
 
   return (
     <>
-      
+      <div className={styles.App}>
+        Test
+      </div>
     </>
   )
 }
