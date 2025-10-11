291e9cf1037c26c4feb280f2dc0c8665781afe1a
anwrat <anweshrwt22@gmail.com>
2025-10-11 21:43:25 +0545

    Adding the header white bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@/components/header";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,11 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-950 relative`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-950 relative h-[5000px]`}
       >
         {/* These two divs below are for the blur bg effect at the top */ }
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
         {children}
       </body>
     </html>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function header() {
+export default function Header() {
   return (
-    <div>
-      
-    </div>
+    //z-index so header is always on top
+    <header className='z-[999] relative'>
+        <div className='fixed top-0 left-1/2 -translate-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-10 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'></div>
+    </header>
   )
 }
